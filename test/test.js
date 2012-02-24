f0aedb132791c49db2c32575fd3b829730650b9d
TheFly <the_fly@dev-VirtualBox.(none)>
2012-02-24 05:00:42 +0000

    Quick and dirty test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,60 @@
+// TODO: Nodeunit
+
 var zpipe = require("../src/zpipe.js");
+var zlib = require("zlib");
 
 var data = "Experiments With Alternating Currents of Very High Frequency, and Their Application to Methods of Artificial Illumination";
 
-var deflated = zpipe.deflate(data);
+var zpipe_deflated = zpipe.deflate(data);
 
-console.log(deflated, deflated.length);
+var zpipe_inflated = zpipe.inflate(zpipe_deflated);
 
-var inflated = zpipe.inflate(deflated);
+var zlib_deflated, zlib_inflated;
 
-console.log(inflated, inflated.length);
+var zlib_inflated_zpipe_deflated, zpipe_inflated_zlib_deflated;
+
+zlib.deflate(data, function(err, buffer) {
+	if(!err) {
+		zlib_deflated = buffer.toString();
+
+		zlib.inflate(buffer, function(err, buffer) {
+			if(!err) {
+				zlib_inflated = buffer.toString();
+
+				zlib.inflate(zpipe_deflated, function(err, buffer) {
+					if(!err) {
+						zlib_inflated_zpipe_deflated = buffer.toString();
+
+						zpipe_inflated_zlib_deflated = zpipe.inflate(zlib_deflated);
+						
+						showResults();
+					} else { 
+						throw err;
+					}
+				});
+			} else {
+				throw err;
+			}
+		});
+	} else {
+		throw err;
+	}
+});
+
+var zlib_inflated = zlib.Inflate(zlib_inflated);
+
+function showResults() {
+	console.log("--------------------------------------------");
+	console.log(zpipe_deflated);
+	console.log("--------------------------------------------");
+	console.log(zpipe_inflated);
+	console.log("--------------------------------------------");
+	console.log(zlib_deflated);
+	console.log("--------------------------------------------");
+	console.log(zlib_inflated);
+	console.log("--------------------------------------------");
+	console.log(zlib_inflated_zpipe_deflated);
+	console.log("--------------------------------------------");
+	console.log(zpipe_inflated_zlib_deflated);
+	console.log("--------------------------------------------");
+};
